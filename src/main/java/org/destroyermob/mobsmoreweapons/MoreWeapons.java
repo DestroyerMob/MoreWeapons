ecce4585d3ece79fb776cd008689e7520aef5c0e
Ethan Hellyer <ethanhellyer7@gmail.com>
2024-06-18 18:16:40 +1000

    Init

--- src/main/java/org/destroyermob/mobsmoreweapons/MoreWeapons.java
+++ src/main/java/org/destroyermob/mobsmoreweapons/MoreWeapons.java
@@ -26,7 +26,6 @@ public class MoreWeapons {
 
     public MoreWeapons() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        // Test
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
