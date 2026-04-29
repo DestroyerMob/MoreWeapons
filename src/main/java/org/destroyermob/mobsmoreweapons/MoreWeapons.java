39f52bb2f82265e1d353e9be1f33bae32e9f4552
DestroyerMob <ethanhellyer7@gmail.com>
2026-04-30 03:52:47 +1000

    Ported to 1.21.1 NeoForge

--- src/main/java/org/destroyermob/mobsmoreweapons/MoreWeapons.java
+++ src/main/java/org/destroyermob/mobsmoreweapons/MoreWeapons.java
@@ -1,43 +1,30 @@
 package org.destroyermob.mobsmoreweapons;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.client.renderer.entity.ThrownTridentRenderer;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.destroyermob.mobsmoreweapons.item.ModItems;
-import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(MoreWeapons.MOD_ID)
 public class MoreWeapons {
 
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "mobsmoreweapons";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
-    public MoreWeapons() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+    public MoreWeapons(IEventBus modEventBus) {
         ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
         // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        NeoForge.EVENT_BUS.register(this);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -96,12 +83,4 @@ public class MoreWeapons {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
-    }
 }
--- src/main/java/org/destroyermob/mobsmoreweapons/item/ModItems.java
+++ src/main/java/org/destroyermob/mobsmoreweapons/item/ModItems.java
@@ -1,89 +1,96 @@
 package org.destroyermob.mobsmoreweapons.item;
 
-import net.minecraft.world.item.*;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.destroyermob.mobsmoreweapons.MoreWeapons;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, MoreWeapons.MOD_ID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MoreWeapons.MOD_ID);
 
     // Greatswords
-    public static final RegistryObject<Item> WOODENGREATSWORD = ITEMS.register("wooden_great_sword",
-            () -> new SwordItem(Tiers.WOOD, 12, -3.5f, new Item.Properties()));
-    public static final RegistryObject<Item> STONEGREATSWORD = ITEMS.register("stone_great_sword",
-            () -> new SwordItem(Tiers.STONE, 12, -3.5f, new Item.Properties()));
-    public static final RegistryObject<Item> IRONGREATSWORD = ITEMS.register("iron_great_sword",
-            () -> new SwordItem(Tiers.IRON, 12, -3.5f, new Item.Properties()));
-    public static final RegistryObject<Item> GOLDGREATSWORD = ITEMS.register("golden_great_sword",
-            () -> new SwordItem(Tiers.GOLD, 12, -3.5f, new Item.Properties()));
-    public static final RegistryObject<Item> DIAMONDGREATSWORD = ITEMS.register("diamond_great_sword",
-            () -> new SwordItem(Tiers.DIAMOND, 12, -3.5f, new Item.Properties()));
-    public static final RegistryObject<Item> NETHERITEGREATSWORD = ITEMS.register("netherite_great_sword",
-            () -> new SwordItem(Tiers.NETHERITE, 12, -3.5f, new Item.Properties()));
+    public static final DeferredItem<Item> WOODENGREATSWORD = ITEMS.register("wooden_great_sword",
+            () -> new SwordItem(Tiers.WOOD, weaponProperties(Tiers.WOOD, 12, -3.5f)));
+    public static final DeferredItem<Item> STONEGREATSWORD = ITEMS.register("stone_great_sword",
+            () -> new SwordItem(Tiers.STONE, weaponProperties(Tiers.STONE, 12, -3.5f)));
+    public static final DeferredItem<Item> IRONGREATSWORD = ITEMS.register("iron_great_sword",
+            () -> new SwordItem(Tiers.IRON, weaponProperties(Tiers.IRON, 12, -3.5f)));
+    public static final DeferredItem<Item> GOLDGREATSWORD = ITEMS.register("golden_great_sword",
+            () -> new SwordItem(Tiers.GOLD, weaponProperties(Tiers.GOLD, 12, -3.5f)));
+    public static final DeferredItem<Item> DIAMONDGREATSWORD = ITEMS.register("diamond_great_sword",
+            () -> new SwordItem(Tiers.DIAMOND, weaponProperties(Tiers.DIAMOND, 12, -3.5f)));
+    public static final DeferredItem<Item> NETHERITEGREATSWORD = ITEMS.register("netherite_great_sword",
+            () -> new SwordItem(Tiers.NETHERITE, weaponProperties(Tiers.NETHERITE, 12, -3.5f)));
 
     // Katanas
-    public static final RegistryObject<Item> WOODENKATANA = ITEMS.register("wooden_katana",
-            () -> new SwordItem(Tiers.WOOD, 2, -2.1f, new Item.Properties()));
-    public static final RegistryObject<Item> STONEKATANA = ITEMS.register("stone_katana",
-            () -> new SwordItem(Tiers.STONE, 2, -2.1f, new Item.Properties()));
-    public static final RegistryObject<Item> IRONKATANA = ITEMS.register("iron_katana",
-            () -> new SwordItem(Tiers.IRON, 2, -2.1f, new Item.Properties()));
-    public static final RegistryObject<Item> GOLDKATANA = ITEMS.register("golden_katana",
-            () -> new SwordItem(Tiers.GOLD, 2, -2.1f, new Item.Properties()));
-    public static final RegistryObject<Item> DIAMONDKATANA = ITEMS.register("diamond_katana",
-            () -> new SwordItem(Tiers.DIAMOND, 2, -2.1f, new Item.Properties()));
-    public static final RegistryObject<Item> NETHERITEKATANA = ITEMS.register("netherite_katana",
-            () -> new SwordItem(Tiers.NETHERITE, 2, -2.1f, new Item.Properties()));
+    public static final DeferredItem<Item> WOODENKATANA = ITEMS.register("wooden_katana",
+            () -> new SwordItem(Tiers.WOOD, weaponProperties(Tiers.WOOD, 2, -2.1f)));
+    public static final DeferredItem<Item> STONEKATANA = ITEMS.register("stone_katana",
+            () -> new SwordItem(Tiers.STONE, weaponProperties(Tiers.STONE, 2, -2.1f)));
+    public static final DeferredItem<Item> IRONKATANA = ITEMS.register("iron_katana",
+            () -> new SwordItem(Tiers.IRON, weaponProperties(Tiers.IRON, 2, -2.1f)));
+    public static final DeferredItem<Item> GOLDKATANA = ITEMS.register("golden_katana",
+            () -> new SwordItem(Tiers.GOLD, weaponProperties(Tiers.GOLD, 2, -2.1f)));
+    public static final DeferredItem<Item> DIAMONDKATANA = ITEMS.register("diamond_katana",
+            () -> new SwordItem(Tiers.DIAMOND, weaponProperties(Tiers.DIAMOND, 2, -2.1f)));
+    public static final DeferredItem<Item> NETHERITEKATANA = ITEMS.register("netherite_katana",
+            () -> new SwordItem(Tiers.NETHERITE, weaponProperties(Tiers.NETHERITE, 2, -2.1f)));
 
     // Battle Axes
-    public static final RegistryObject<Item> WOODENBATTLEAXE = ITEMS.register("wooden_battle_axe",
-            () -> new SwordItem(Tiers.WOOD, 7, -3f, new Item.Properties()));
-    public static final RegistryObject<Item> STONEBATTLEAXE = ITEMS.register("stone_battle_axe",
-            () -> new SwordItem(Tiers.STONE, 8, -3f, new Item.Properties()));
-    public static final RegistryObject<Item> IRONBATTLEAXE = ITEMS.register("iron_battle_axe",
-            () -> new SwordItem(Tiers.IRON, 7, -3f, new Item.Properties()));
-    public static final RegistryObject<Item> GOLDBATTLEAXE = ITEMS.register("golden_battle_axe",
-            () -> new SwordItem(Tiers.GOLD, 7, -3f, new Item.Properties()));
-    public static final RegistryObject<Item> DIAMONDBATTLEAXE = ITEMS.register("diamond_battle_axe",
-            () -> new SwordItem(Tiers.DIAMOND, 6, -3f, new Item.Properties()));
-    public static final RegistryObject<Item> NETHERITEBATTLEAXE = ITEMS.register("netherite_battle_axe",
-            () -> new SwordItem(Tiers.NETHERITE, 6, -3f, new Item.Properties()));
+    public static final DeferredItem<Item> WOODENBATTLEAXE = ITEMS.register("wooden_battle_axe",
+            () -> new SwordItem(Tiers.WOOD, weaponProperties(Tiers.WOOD, 7, -3f)));
+    public static final DeferredItem<Item> STONEBATTLEAXE = ITEMS.register("stone_battle_axe",
+            () -> new SwordItem(Tiers.STONE, weaponProperties(Tiers.STONE, 8, -3f)));
+    public static final DeferredItem<Item> IRONBATTLEAXE = ITEMS.register("iron_battle_axe",
+            () -> new SwordItem(Tiers.IRON, weaponProperties(Tiers.IRON, 7, -3f)));
+    public static final DeferredItem<Item> GOLDBATTLEAXE = ITEMS.register("golden_battle_axe",
+            () -> new SwordItem(Tiers.GOLD, weaponProperties(Tiers.GOLD, 7, -3f)));
+    public static final DeferredItem<Item> DIAMONDBATTLEAXE = ITEMS.register("diamond_battle_axe",
+            () -> new SwordItem(Tiers.DIAMOND, weaponProperties(Tiers.DIAMOND, 6, -3f)));
+    public static final DeferredItem<Item> NETHERITEBATTLEAXE = ITEMS.register("netherite_battle_axe",
+            () -> new SwordItem(Tiers.NETHERITE, weaponProperties(Tiers.NETHERITE, 6, -3f)));
 
     // Knives
-    public static final RegistryObject<Item> WOODENKNIFE = ITEMS.register("wooden_knife",
-            () -> new SwordItem(Tiers.WOOD, 1, -2f, new Item.Properties()));
-    public static final RegistryObject<Item> STONEKNIFE = ITEMS.register("stone_knife",
-            () -> new SwordItem(Tiers.STONE, 1, -2f, new Item.Properties()));
-    public static final RegistryObject<Item> IRONKNIFE = ITEMS.register("iron_knife",
-            () -> new SwordItem(Tiers.IRON, 1, -2f, new Item.Properties()));
-    public static final RegistryObject<Item> GOLDENKNIFE = ITEMS.register("golden_knife",
-            () -> new SwordItem(Tiers.GOLD, 1, -2f, new Item.Properties()));
-    public static final RegistryObject<Item> DIAMONDKNIFE = ITEMS.register("diamond_knife",
-            () -> new SwordItem(Tiers.DIAMOND, 1, -2f, new Item.Properties()));
-    public static final RegistryObject<Item> NETHERITEKNIFE = ITEMS.register("netherite_knife",
-            () -> new SwordItem(Tiers.NETHERITE, 1, -2f, new Item.Properties()));
+    public static final DeferredItem<Item> WOODENKNIFE = ITEMS.register("wooden_knife",
+            () -> new SwordItem(Tiers.WOOD, weaponProperties(Tiers.WOOD, 1, -2f)));
+    public static final DeferredItem<Item> STONEKNIFE = ITEMS.register("stone_knife",
+            () -> new SwordItem(Tiers.STONE, weaponProperties(Tiers.STONE, 1, -2f)));
+    public static final DeferredItem<Item> IRONKNIFE = ITEMS.register("iron_knife",
+            () -> new SwordItem(Tiers.IRON, weaponProperties(Tiers.IRON, 1, -2f)));
+    public static final DeferredItem<Item> GOLDENKNIFE = ITEMS.register("golden_knife",
+            () -> new SwordItem(Tiers.GOLD, weaponProperties(Tiers.GOLD, 1, -2f)));
+    public static final DeferredItem<Item> DIAMONDKNIFE = ITEMS.register("diamond_knife",
+            () -> new SwordItem(Tiers.DIAMOND, weaponProperties(Tiers.DIAMOND, 1, -2f)));
+    public static final DeferredItem<Item> NETHERITEKNIFE = ITEMS.register("netherite_knife",
+            () -> new SwordItem(Tiers.NETHERITE, weaponProperties(Tiers.NETHERITE, 1, -2f)));
 
     // Machetes
-    public static final RegistryObject<Item> WOODENMACHETE = ITEMS.register("wooden_machete",
-            () -> new SwordItem(Tiers.WOOD, 5, -2.7f, new Item.Properties()));
-    public static final RegistryObject<Item> STONEMACHETE = ITEMS.register("stone_machete",
-            () -> new SwordItem(Tiers.STONE, 5, -2.7f, new Item.Properties()));
-    public static final RegistryObject<Item> IRONMACHETE = ITEMS.register("iron_machete",
-            () -> new SwordItem(Tiers.IRON, 5, -2.7f, new Item.Properties()));
-    public static final RegistryObject<Item> GOLDENMACHETE = ITEMS.register("golden_machete",
-            () -> new SwordItem(Tiers.GOLD, 5, -2.7f, new Item.Properties()));
-    public static final RegistryObject<Item> DIAMONDMACHETE = ITEMS.register("diamond_machete",
-            () -> new SwordItem(Tiers.DIAMOND, 5, -2.7f, new Item.Properties()));
-    public static final RegistryObject<Item> NETHERITEMACHETE = ITEMS.register("netherite_machete",
-            () -> new SwordItem(Tiers.NETHERITE, 5, -2.7f, new Item.Properties()));
+    public static final DeferredItem<Item> WOODENMACHETE = ITEMS.register("wooden_machete",
+            () -> new SwordItem(Tiers.WOOD, weaponProperties(Tiers.WOOD, 5, -2.7f)));
+    public static final DeferredItem<Item> STONEMACHETE = ITEMS.register("stone_machete",
+            () -> new SwordItem(Tiers.STONE, weaponProperties(Tiers.STONE, 5, -2.7f)));
+    public static final DeferredItem<Item> IRONMACHETE = ITEMS.register("iron_machete",
+            () -> new SwordItem(Tiers.IRON, weaponProperties(Tiers.IRON, 5, -2.7f)));
+    public static final DeferredItem<Item> GOLDENMACHETE = ITEMS.register("golden_machete",
+            () -> new SwordItem(Tiers.GOLD, weaponProperties(Tiers.GOLD, 5, -2.7f)));
+    public static final DeferredItem<Item> DIAMONDMACHETE = ITEMS.register("diamond_machete",
+            () -> new SwordItem(Tiers.DIAMOND, weaponProperties(Tiers.DIAMOND, 5, -2.7f)));
+    public static final DeferredItem<Item> NETHERITEMACHETE = ITEMS.register("netherite_machete",
+            () -> new SwordItem(Tiers.NETHERITE, weaponProperties(Tiers.NETHERITE, 5, -2.7f)));
 
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
+    }
+
+    private static Item.Properties weaponProperties(Tier tier, int attackDamage, float attackSpeed) {
+        return new Item.Properties()
+                .durability(tier.getUses())
+                .attributes(SwordItem.createAttributes(tier, attackDamage, attackSpeed));
     }
 
 }
